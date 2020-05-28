0b906ce18fd6d52d2a712dbc60e7dc104c3959b7
yonechitest <yonetomo.asics@gmail.com>
2020-05-28 19:38:41 +0900

    complete Ver1.0

--- resources/views/layouts/calcu_app.blade.php
+++ resources/views/layouts/calcu_app.blade.php
@@ -3,6 +3,7 @@
         <title>@yield('title')</title>
         <link rel="stylesheet" href="{{ asset('css/calcu.css') }}">
         <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
     </head>
     <body>
         @section('display')
