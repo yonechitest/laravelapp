f92c9da68903e19f9174be3ec7563269674420ae
yonechitest <yonetomo.asics@gmail.com>
2020-06-30 00:02:11 +0900

    install laravelUI ReactJs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,23 @@
-@extends('adminlte::page')
-
-@section('title', 'Dashboard')
-
-@section('content_header')
-    <h1>Dashboard</h1>
-@stop
+@extends('layouts.app')
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
-@stop
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
 
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
-@stop
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
-@section('js')
-    <script> console.log('Hi!'); </script>
-@stop
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
